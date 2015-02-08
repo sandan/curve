5b381b785ac3962a04417f2543d8a1e3b846da10
bretts61 <bretts61@gmail.com>
2015-02-08 16:31:23 -0600

    Created benchmark for Z3 zrange

--- benchmarks/src/main/scala/org/locationtech/curve/benchmarks/ZCurveBenchmark.scala
+++ benchmarks/src/main/scala/org/locationtech/curve/benchmarks/ZCurveBenchmark.scala
@@ -15,6 +15,10 @@ class ZCurveBenchmark extends CurveBenchmark {
   val ys3 = (0 until 200).toArray
   val zs3 = (0 until 200).toArray
 
+  val xs3l = (200 until 400).toArray
+  val ys3l = (200 until 400).toArray
+  val zs3l = (200 until 400).toArray
+
   def timeZ2IndexCreate(reps: Int) = run(reps)(z2IndexCreation)
   def z2IndexCreation = {
     var x = 0
@@ -45,5 +49,25 @@ class ZCurveBenchmark extends CurveBenchmark {
       }
       x += 1
     }
+  }
+
+  def timeZ3ZRanges(reps: Int) = run(reps)(z3ZRangesCreation)
+  def z3ZRangesCreation = {
+      var x = 0
+      var y = 0
+      var z = 0
+
+      while(x < 100){
+          while(y < 100){
+              while(z < 100){
+                  var z31 = Z3(xs3(x), ys3(y), zs3(z))
+                  var z32 = Z3(xs3l(x), ys3l(y), zs3l(z))
+                  Z3.zranges(z31, z32)
+                  z += 1
+              }
+              y += 1
+          }
+          x += 1
+      }
   }
 }
