454a021a982b01ab76dd57a10b1c3dbb86830b4f
Mark Sandan <msandan@utexas.edu>
2015-03-05 12:43:47 -0600

    interface for space filling curves

--- interface/src/main/scala/org/locationtech/sfc/SpaceFillingCurve.scala
+++ interface/src/main/scala/org/locationtech/sfc/SpaceFillingCurve.scala
@@ -1,8 +1,9 @@
 package org.locationtech.sfc
+/**
+ * Interface for space filling curves. A first approximation.
+ **/
 trait SpaceFillingCurve {
-  def PointToValue(point: Coordinate): Long
-  
-  def ValueToPoint(value: Long): Coordinate
-  
-  def RangeQuery(lowerLeft: Coordinate, upperRight: Coordinate)
+  def PointToValue(x: Double, y: Double): Long
+  def ValueToPoint(value: Long): (Double, Double)
+  def RangeQuery(ll_x: Double, ll_y: Double, ur_x: Double, ur_y: Double)
 }
