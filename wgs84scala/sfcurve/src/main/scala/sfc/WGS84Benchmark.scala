23f6a4f34e63281ed4cd5f2ede60b769d887146f
bretts61 <bretts61@gmail.com>
2015-03-12 23:29:16 -0500

    Added hilbert curve benchmark file

---
+++ wgs84scala/sfcurve/src/main/scala/sfc/WGS84Benchmark.scala
@@ -0,0 +1,39 @@
+package sfc
+
+
+
+import com.google.caliper.Param
+
+object WGS84Benchmark extends BenchmarkRunner(classOf[WGS84Benchmark])
+
+class WGS84Benchmark extends CurveBenchmark {
+
+
+  /*def timeWGS84IndexCreate(reps: Int) = run(reps)(WGS84IndexCreation)
+  def WGS84IndexCreation = {
+    var x = -180
+    var y = -90
+
+    while(x < 180) {
+      while(y < 90) {
+        HilbertCurve(WGS84Coordinate(x),y)
+        y += 1
+      }
+      x += 1
+    }
+}*/
+
+  def timeWGS84BadCase(reps: Int) = run(reps)(WGS84BadCase)
+  def WGS84BadCase = {
+
+      val i = 2 //resolution bits
+      while (i < 24){
+          val sfc = HilbertCurve(i)
+          //how to do list?
+          val range = sfc.RangeQuery(WGS84Coordinate(-178.123456, -86.398493), WGSCoordinate(179.3211113, 87.393483))
+          ++i
+      }
+
+  }
+
+}
