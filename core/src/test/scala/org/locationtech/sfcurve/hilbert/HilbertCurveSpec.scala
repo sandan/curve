0d8b11585a34774fbe35ed06ebdf68dd6ea920af
Mark Sandan <msandan@utexas.edu>
2015-03-21 18:23:20 -0500

    update test structure

--- core/src/test/scala/org/locationtech/sfcurve/hilbert/HilbertCurveSpec.scala
+++ core/src/test/scala/org/locationtech/sfcurve/hilbert/HilbertCurveSpec.scala
@@ -3,7 +3,17 @@ package org.locationtech.curve.hilbertCurve
 import org.scalatest._
 
 class HilbertCurveSpec extends FunSpec with Matchers {
+
+  val EPSILON: Double = 1E-3
+
   describe("A HilbertCurve implementation using UG lib") {
+
+    it("translates (Double,Double) to Long and Long to (Double, Double)"){
+      val sfc = HilbertCurve(20)
+      val index = sfc.PointToValue(0.0,0.0)
+      sfc.ValueToPoint(index) should be < (0.0, 0.0)
+      sfc.ValueToPoint(index) should be > (-EPSILON, -EPSILON)
+    }
 
     it("implements a range query"){
 
