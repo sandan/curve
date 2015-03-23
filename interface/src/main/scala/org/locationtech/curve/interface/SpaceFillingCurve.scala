667474fd2a2d50c9dcfb11637f69594e16aef8f2
Mark Sandan <msandan@utexas.edu>
2015-03-23 10:06:43 -0500

    remove the interface/ subproject and HilbertCurve Coordinate implementations

--- interface/src/main/scala/org/locationtech/curve/interface/SpaceFillingCurve.scala
+++
@@ -1,9 +0,0 @@
-package org.locationtech.curve.interface
-/**
- * Interface for space filling curves. A first approximation.
- **/
-trait SpaceFillingCurve {
-  def PointToValue(x: Double, y: Double): Long
-  def ValueToPoint(value: Long): (Double, Double)
-  def RangeQuery(ll_x: Double, ll_y: Double, ur_x: Double, ur_y: Double):List[Array[Long]]
-}
